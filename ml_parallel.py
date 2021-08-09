ce707d60db2b79c6f707740a79e1af8a5948c091
Wataru Kinjo <e195764@ie.u-ryukyu.ac.jp>
2021-08-09 17:09:17 +0900

    modified ml_serial_def.py and ml_parallel.py

--- ml_parallel.py
+++ ml_parallel.py
@@ -2,64 +2,52 @@
 import pandas as pd
 import numpy as np
 from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score # 正解率(accuracy)
-from sklearn.metrics import precision_score # 適合率(precision)
-from sklearn.metrics import recall_score # 検出率(recall)
 from sklearn.metrics import f1_score # F値
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-import time
-import threading
+import concurrent.futures
 
 # load the dataset
-natural_data = pd.read_csv('heart_failure_clinical_records_dataset.csv')
+dataset_org = pd.read_csv('heart_failure_clinical_records_dataset.csv')
 
-def conv_matrix(natural_data):
-    dataset = natural_data.to_numpy()
-    np.set_printoptions(suppress=True, precision=2)
-    return dataset
+# Convert to the matrix
+dataset = dataset_org.to_numpy()
+np.set_printoptions(suppress=True, precision=2)
 
-def create_features_matrix(dataset):
-    X = dataset[:, 0:12]
-    sc = StandardScaler()
-    X_std = sc.fit_transform(X)
-    return X_std
+# Create the new matrix without test-data
+X = dataset[:,0:12]
 
-def create_test_matrix(dataset):
-    y = dataset[:, 12]
-    return y
+# standardize X
+sc = StandardScaler()
+X_std = sc.fit_transform(X)
 
-"""
-thread1 = threading.Thread(target=create_features_matrix(conv_matrix(natural_data)))
-thread2 = threading.Thread(target=create_test_matrix(conv_matrix(natural_data)))
-thread1.start()
-thread2.start()
-thread1.join()
-thread2.join()
-"""
+# Create the test-data
+y = dataset[:,12]
 
 # Split the dataset into training and testing set(sklearn)
-X_train, X_test, y_train, y_test = train_test_split(create_features_matrix(conv_matrix(natural_data)), create_test_matrix(conv_matrix(natural_data)), test_size = 0.5)
+X_train, X_test, y_train, y_test = train_test_split(X_std, y, test_size = 0.5)
 
 
-def ml(X_train, y_train, X_test):
+def LinSVC():
     lin_clf = svm.LinearSVC(tol=1e-5, max_iter=10000)
     lin_clf.fit(X_train, y_train)
     predicted = lin_clf.predict(X_test)
+    print("LinearSVCの正解率 = " + str('{:.2f}'.format(accuracy_score(y_test, predicted))))
+    print("LinearSVCのF値 = " + str('{:.2f}'.format(f1_score(y_test, predicted))))
     return predicted
 
-predicted = ml(X_train, y_train,X_test)
+def KNN():
+    knn_clf = KNeighborsClassifier()
+    knn_clf.fit(X_train, y_train)
+    predicted = knn_clf.predict(X_test)
+    print("KNNの正解率 = " + str('{:.2f}'.format(accuracy_score(y_test, predicted))))
+    print("KNNのF値 = " + str('{:.2f}'.format(f1_score(y_test, predicted))))
+    return predicted
 
 
-def index(y_test, predicted):
-    print("正解率 = " + str(accuracy_score(y_test, predicted)))
-    print("適合率 = " + str(precision_score(y_test, predicted)))
-    print("検出率 = " + str(recall_score(y_test, predicted)))
-    print("F値 = " + str(f1_score(y_test, predicted)))
-
-
-index(y_test, predicted)
-
-
-
-
+if __name__ == "__main__":
+    executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
+    executor.submit(LinSVC)
+    executor.submit(KNN)
--- ml_serial_def.py
+++ ml_serial_def.py
@@ -2,9 +2,8 @@
 import pandas as pd
 import numpy as np
 from sklearn import svm
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score # 正解率(accuracy)
-from sklearn.metrics import precision_score # 適合率(precision)
-from sklearn.metrics import recall_score # 検出率(recall)
 from sklearn.metrics import f1_score # F値
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
@@ -13,50 +12,40 @@ from sklearn.model_selection import train_test_split
 # load the dataset
 dataset_org = pd.read_csv('heart_failure_clinical_records_dataset.csv')
 
-def conv_matrix(dataset_org):
-    dataset = dataset_org.to_numpy()
-    np.set_printoptions(suppress=True, precision=2)
-    return dataset
+# Convert to the matrix
+dataset = dataset_org.to_numpy()
+np.set_printoptions(suppress=True, precision=2)
 
-dataset = conv_matrix(dataset_org)
-#print(dataset[:5])
+# Create the new matrix without test-data
+X = dataset[:,0:12]
 
+# standardize X
+sc = StandardScaler()
+X_std = sc.fit_transform(X)
 
-def create_features_matrix(dataset):
-    X = dataset[:, 0:12]
-    sc = StandardScaler()
-    X_std = sc.fit_transform(X)
-    return X_std
-
-#print(create_features_matrix(dataset)[:5])
-
-
-def create_test_matrix(dataset):
-    y = dataset[:, 12]
-    return y
-
-#print(create_test_matrix(dataset)[:5])
-
+# Create the test-data
+y = dataset[:,12]
 
 # Split the dataset into training and testing set(sklearn)
-X_train, X_test, y_train, y_test = train_test_split(create_features_matrix(dataset), create_test_matrix(dataset), test_size = 0.5)
+X_train, X_test, y_train, y_test = train_test_split(X_std, y, test_size = 0.5)
 
 
-def ml(X_train, y_train, X_test):
+def LinSVC():
     lin_clf = svm.LinearSVC(tol=1e-5, max_iter=10000)
     lin_clf.fit(X_train, y_train)
     predicted = lin_clf.predict(X_test)
+    print("LinearSVCの正解率 = " + str('{:.2f}'.format(accuracy_score(y_test, predicted))))
+    print("LinearSVCのF値 = " + str('{:.2f}'.format(f1_score(y_test, predicted))))
     return predicted
 
-predicted = ml(X_train, y_train,X_test)
+def KNN():
+    knn_clf = KNeighborsClassifier()
+    knn_clf.fit(X_train, y_train)
+    predicted = knn_clf.predict(X_test)
+    print("KNNの正解率 = " + str('{:.2f}'.format(accuracy_score(y_test, predicted))))
+    print("KNNのF値 = " + str('{:.2f}'.format(f1_score(y_test, predicted))))
+    return predicted
 
 
-def index(y_test, predicted):
-    print("正解率 = " + str(accuracy_score(y_test, predicted)))
-    print("適合率 = " + str(precision_score(y_test, predicted)))
-    print("検出率 = " + str(recall_score(y_test, predicted)))
-    print("F値 = " + str(f1_score(y_test, predicted)))
-
-
-index(y_test, predicted)
-
+predicted_LinSVC = LinSVC()
+predicted_KNN = KNN()
